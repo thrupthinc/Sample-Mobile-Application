848ce08ebf28a2964e3c4b1c9f93527cab38a0d2
unknown <E0066@IST-12120047.ISTMYSPDN02.com>
2015-04-27 16:28:17 +0530

    commit

--- hello/www/js/index.js
+++ hello/www/js/index.js
@@ -35,7 +35,7 @@ function callSoapHeader() {
     //xmlhttp.open('POST', 'http://localhost:57564/EmployeeData.asmx?op=HelloWorld', true);
     var sr='<?xml version="1.0" encoding="utf-8"?>'+
            '<soap:Envelope'+
-           ' xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"'+
+           'xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"'+
             'xmlns:xsd="http://www.w3.org/2001/XMLSchema"'+
             'xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">'+
             '<soap:Body>'+
