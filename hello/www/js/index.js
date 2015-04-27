dec596aa60c7d2acfe93b0c15c292ee0dadf66c3
unknown <E0066@IST-12120047.ISTMYSPDN02.com>
2015-04-27 17:51:00 +0530

    commit

--- hello/www/js/index.js
+++ hello/www/js/index.js
@@ -30,90 +30,91 @@
 //        console.log('Received Event: ' + id);
 //    }
 //};
-function callSoapHeader() {
-    //var xmlhttp = new XMLHttpRequest();
-    //xmlhttp.open('POST', 'http://localhost:57564/EmployeeData.asmx?op=HelloWorld', true);
-    var sr='<?xml version="1.0" encoding="utf-8"?>'+
-           '<soap:Envelope'+
-           'xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"'+
-            'xmlns:xsd="http://www.w3.org/2001/XMLSchema"'+
-            'xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">'+
-            '<soap:Body>'+
-            '<HelloWorld xmlns="http://tempuri.org/" />'+
-            '</soap:Body>'+
-            '<soap:Envelope>';
+//function callSoapHeader() {
+//    //var xmlhttp = new XMLHttpRequest();
+//    //xmlhttp.open('POST', 'http://localhost:57564/EmployeeData.asmx?op=HelloWorld', true);
+//    var sr='<?xml version="1.0" encoding="utf-8"?>'+
+//           '<soap:Envelope'+
+//           'xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"'+
+//            'xmlns:xsd="http://www.w3.org/2001/XMLSchema"'+
+//            'xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">'+
+//            '<soap:Body>'+
+//            '<HelloWorld xmlns="http://tempuri.org/" />'+
+//            '</soap:Body>'+
+//            '<soap:Envelope>';
 
-    //xmlhttp.onreadystatechange = function () {
-    //    if (xmlhttp.readyState == 4) {
-    //        if (xmlhttp.status == 200) {
+//    //xmlhttp.onreadystatechange = function () {
+//    //    if (xmlhttp.readyState == 4) {
+//    //        if (xmlhttp.status == 200) {
 
-    //            alert('Sorry page coud not found');
-    //        }
-    //    }
-    //}
-    //xmlhttp.setRequestHeader('Content-Type', 'text/xml');
-    //xmlhttp.send(sr);
-    return sr;
-}
-function callback(result) {
-    alert(result);
-    if (result) {
-        alert("SUCCESS");
-    }
-    else {
-        alert("Error");
-    }
-}
-function WebSvc()  {
-    WebSvc.prototype.CallWebService = function (url, soapXml, callback) {
-        var xmlDoc = null;
-        if (window.XMLHttpRequest) {
-            xmlDoc = new XMLHttpRequest(); 
-        }
-        else if (window.ActiveXObject) //IE 5, 6
-        {
-            xmlDoc = new ActiveXObject("Microsoft.XMLHTTP");
-        }
-        alert(xmlDoc);
-        if (xmlDoc) {
-            //callback for readystate when it returns
-            var self = this;
-            xmlDoc.onreadystatechange = function () { self.StateChange(xmlDoc, callback); };
+//    //            alert('Sorry page coud not found');
+//    //        }
+//    //    }
+//    //}
+//    //xmlhttp.setRequestHeader('Content-Type', 'text/xml');
+//    //xmlhttp.send(sr);
+//    return sr;
+//}
+//function callback(result) {
+//    alert(result);
+//    if (result) {
+//        alert("SUCCESS");
+//    }
+//    else {
+//        alert("Error");
+//    }
+//}
+//function WebSvc()  {
+//    WebSvc.prototype.CallWebService = function (url, soapXml, callback) {
+//        var xmlDoc = null;
+//        if (window.XMLHttpRequest) {
+//            xmlDoc = new XMLHttpRequest(); 
+//        }
+//        else if (window.ActiveXObject) //IE 5, 6
+//        {
+//            xmlDoc = new ActiveXObject("Microsoft.XMLHTTP");
+//        }
+//        alert(xmlDoc);
+//        if (xmlDoc) {
+//            //callback for readystate when it returns
+//            var self = this;
+//            xmlDoc.onreadystatechange = function () { self.StateChange(xmlDoc, callback); };
 
-            //set up the soap xml web service call
-            xmlDoc.open("POST", url, true);
-            xmlDoc.setRequestHeader("Content-Type", "text/xml");
-            xmlDoc.setRequestHeader("Content-Length", soapXml.length);
-            xmlDoc.send(soapXml);
-        }
-        else {
-            if (callback) {
-                callback(false);
-            }
-        }
-    };
+//            //set up the soap xml web service call
+//            xmlDoc.open("POST", url, true);
+//            xmlDoc.setRequestHeader("Content-Type", "text/xml");
+//            xmlDoc.setRequestHeader("Content-Length", soapXml.length);
+//            xmlDoc.send(soapXml);
+//        }
+//        else {
+//            if (callback) {
+//                callback(false);
+//            }
+//        }
+//    };
 
-    WebSvc.prototype.StateChange = function (xmlDoc, callback) {
-        if (xmlDoc.readyState === 4) {
-            var text = "";
+//    WebSvc.prototype.StateChange = function (xmlDoc, callback) {
+//        if (xmlDoc.readyState === 4) {
+//            var text = "";
 
-            if (xmlDoc.status === 200) {
-                text = xmlDoc.responseText;
-            }
-            if (callback !== null) {
-                callback(xmlDoc.status === 200, text);
-            }
-        }
-    };
-}
+//            if (xmlDoc.status === 200) {
+//                text = xmlDoc.responseText;
+//            }
+//            if (callback !== null) {
+//                callback(xmlDoc.status === 200, text);
+//            }
+//        }
+//    };
+//}
 
-function getMessage() {
+//function getMessage() {
 
-    var soap = callSoapHeader();
-    var webServiceCall = new WebSvc();
-    alert(webServiceCall.CallWebService("EmployeeData.asmx?op=HelloWorld", soap, callback));;
-    alert("weBservice call");
-}
+//    var soap = callSoapHeader();
+//    var webServiceCall = new WebSvc();
+//    alert(webServiceCall.CallWebService("EmployeeData.asmx?op=HelloWorld", soap, callback));;
+//    alert("weBservice call");
+//}
+
 
 
 
